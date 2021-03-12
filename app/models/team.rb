656e04a64454876564997596e035002b1f178394
enelesmai <xochitl.selene.fp@gmail.com>
2021-03-12 15:54:34 -0600

    added specs for team model

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,38 +1,36 @@
 class Team
     include ActiveModel::Model
     include Utils
-    attr_reader :name, :expected_scores, :total_scores, :levels_config 
-    attr_accessor :players, :percentage
-    attribute :name, :string
-    attribute :expected_scores, :integer
-    attribute :total_scores, :integer
-    attribute :percentage, :float
+    attr_reader :name, :levels_config 
+    attr_accessor :players, :percentage, :total_scores, :expected_scores
+    validates_presence_of :name, :expected_scores, :total_scores, :levels_config, :players
 
-    # initialization will setup the team and will calculate percentage
+    # initialization will setup the team to prepare it for the calculations
     def initialize(name, players, config)
+        @name = name
         @levels_config = config
         @total_scores = 0
         @expected_scores = 0
-        setup_team(name, players)
-        calculate_percentage
+        setup_team(players) if !players.nil? 
     end
 
     # initialize team players from the original list by filtering 
     # and calculate total scores for that players
-    def setup_team(players, team)
-        @players = players.select { |player| player.equipo == team }
-        @total_scores = players.inject { |sum, n| sum + player.goles }
+    def setup_team(players)
+        @players = players.select { |player| player.equipo == @name }
+        @total_scores = @players.map(&:goles).inject(:+)
     end
 
     # calculate expected score based on the level of each player
     def setup_expected_score
-        @players.each |player| do
-            @expected_scores += get_meta_from_config_list(@config_levels, player.nivel)
+        @players.each do |player|
+            @expected_scores += get_meta_from_config_list(@levels_config, player.nivel)
         end
     end
 
     # percentage value, for example: 32% will be 0.32
     def calculate_percentage
+        setup_expected_score
         @percentage = @total_scores / @expected_scores
     end
 
---
+++ spec/models/team_spec.rb
@@ -0,0 +1,81 @@
+require 'rails_helper'
+
+RSpec.describe 'Team model' do
+
+  let(:players_list) { [Player.new(
+    nombre: 'Daniel', 
+    nivel: 'A', 
+    goles: 5, 
+    sueldo: 20000, 
+    bono: 2500, 
+    equipo: 'Azul'),
+    Player.new(
+    nombre: 'Carlos', 
+    nivel: 'B', 
+    goles: 9, 
+    sueldo: 50000, 
+    bono: 8000, 
+    equipo: 'Azul'),
+    Player.new(
+    nombre: 'Alessa', 
+    nivel: 'C', 
+    goles: 5, 
+    sueldo: 60000, 
+    bono: 10000, 
+    equipo: 'Verde')
+    ]
+  }
+
+  let(:config) {[
+      { nivel: 'A',
+        meta: 10    
+        },
+        {
+            nivel: 'B',
+            meta: 12    
+        },
+        {nivel: 'C',
+        meta: 15    
+    }
+  ]}
+
+  let(:valid_team) { Team.new(
+    'Azul', 
+    players_list,
+    config)}
+
+  describe '#Initialize a team' do
+    context 'when creating a team' do
+      it "is valid with valid attributes" do
+        expect(valid_team).to be_valid
+      end
+      it "is not valid without a name" do
+        team = Team.new(nil, players_list, config)
+        expect(team).to_not be_valid
+      end
+      it "is not valid without players" do
+        team = Team.new('Azul', nil, config)
+        expect(team).to_not be_valid
+      end
+      it "is not valid without levels_config" do
+        team = Team.new('Azul', players_list, nil)
+        expect(team).to_not be_valid
+      end
+      it "keeps only players from the team" do
+        expect(valid_team.players.count).to eq(2)
+      end
+      it "calculates total scores of the team" do
+        expect(valid_team.total_scores).to eq(14)  
+      end
+      it "calculates expected scores of the team" do
+        valid_team.calculate_percentage
+        expect(valid_team.expected_scores).to eq(22)  
+      end
+      it "calculates percentage scored" do
+        valid_team.calculate_percentage
+        expect(valid_team.percentage).to eq(14/22)
+      end
+    end
+  end
+
+end
