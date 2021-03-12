f4f3bf65f54b00fd92327121c7e6d6f810b3d352
enelesmai <xochitl.selene.fp@gmail.com>
2021-03-12 06:02:51 -0600

    added spec example

---
+++ app/controllers/concerns/defaults.rb
@@ -0,0 +1,21 @@
+module Defaults
+    DEFAULT_CONFIG =
+        [
+        {
+        nivel: 'A',
+        meta: 5
+        },
+        {
+        nivel: 'B',
+        meta: 10
+        },
+        {
+        nivel: 'C',
+        meta: 15
+        },
+        {
+        nivel: 'Cuauh',
+        meta: 20
+        }
+    ].freeze
+end
